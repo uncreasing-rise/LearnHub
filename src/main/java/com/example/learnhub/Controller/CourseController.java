20f3e9ea6dddb73b00e72f9b357a94f3ac02814e
Minh Hoang <truonghoangprovip@gmail.com>
2024-03-10 13:54:36 +0700

    Add @CrossOrigin(origins = "*", maxAge = 3600)

--- src/main/java/com/example/learnhub/Controller/CourseController.java
+++ src/main/java/com/example/learnhub/Controller/CourseController.java
@@ -20,7 +20,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
-
+@CrossOrigin(origins = "*", maxAge = 3600)
 @MultipartConfig
 @RestController
 @RequestMapping("/courses")
